d9c8fcf6abe6f6c4f8ca2b2334be0856edbfad9a
Rafael Arantes <birutaibm@gmail.com>
2016-06-16 22:34:42 -0300

    Criada a classe Player. Alguns pequenos ajustes foram feitos nas outras classes. Falta detectar bordas, colisões e água.

--- js/app.js
+++ js/app.js
@@ -1,60 +1,62 @@
 //superCalss de Enemy e Player
-var Entity = function() {};
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Entity.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+var Entity = function(x, y, sprite) {
+   this.x = x;
+   this.y = y;
+   this.velocity = 200 * Math.random() + 50;
+   this.sprite = sprite;
 };
 // Draw the enemy on the screen, required method for game
 Entity.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.getX(), this.getY());
+   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Enemies our player must avoid
-var Enemy = function() {
-    Entity.call(this);
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function(row) {
+   Entity.call(this, 0, row*80-11, 'images/enemy-bug.png');
 };
 Enemy.prototype = Object.create(Entity.prototype);
 Enemy.prototype.constructor = Enemy;
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+   this.x = this.x + this.velocity*dt;
+   if (this.x > 510)
+      this.x = -100;
 };
 
-Enemy.prototype.getX = function() {
-    return 0;
-};
-Enemy.prototype.getY = function() {
-    return 83;
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.getX(), this.getY());
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+   Entity.call(this, 202, 400, 'images/char-boy.png');
+   this.dir = '';
+   this.dx = 80 * Math.random() + 20;
+   this.dy = 80 * Math.random() + 20;
+};
+Player.prototype = Object.create(Entity.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function() {
+   if (this.dir === 'left')
+      this.x -= this.dx;
+   else if (this.dir === 'right')
+      this.x += this.dx;
+   else if (this.dir === 'up')
+      this.y -= this.dy;
+   else if (this.dir === 'down')
+      this.y += this.dy;
+   this.dir = '';
+};
+Player.prototype.handleInput = function(dir) {
+   this.dir = dir;
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy()];
+var allEnemies = [new Enemy(1), new Enemy(2), new Enemy(3)];
 // Place the player object in a variable called player
-player = new Enemy(); //TODO alterar essa linha quando criar a classe Player
-
+var player = new Player(); //TODO alterar essa linha quando criar a classe Player
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
